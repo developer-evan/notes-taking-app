50e4fc8059c1590983e0ef0a6fb754d93915f91c
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-06-27 10:20:19 -0700

    implemented text input field to send data to firebase backend

--- src/NoteAdd.js
+++ src/NoteAdd.js
@@ -1,12 +1,101 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Keyboard,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { firebase } from "../config";
 
 const NoteAdd = () => {
+  const [title, setTitle] = useState("");
+  const [note, setNote] = useState("");
+  const handleAdd = () => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .add({
+        title,
+        note,
+      })
+      .then(() => {
+        setTitle("");
+        setNote("");
+        Keyboard.dismiss();
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
   return (
-    <View>
-      <Text>NoteAdd</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      {/* <Text>NoteAdd</Text> */}
+      <TextInput
+        placeholder="Title"
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        style={styles.inputTitle}
+      />
+      <TextInput
+        placeholder="Note"
+        value={note}
+        onChangeText={(text) => setNote(text)}
+        style={styles.inputNote}
+      />
 
-export default NoteAdd
+      <TouchableOpacity style={styles.button} onPress={handleAdd}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default NoteAdd;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#73fbd3",
+  },
+  inputTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 10,
+    height: 50,
+    width: "97%",
+    borderBottomWidth: 1 / 2,
+    borderLeftWidth: 1 / 2,
+    padding: 10,
+  },
+  inputNote: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 10,
+    height: 200,
+    width: "97%",
+    borderBottomWidth: 1 / 2,
+    borderLeftWidth: 1 / 2,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "red",
+    borderRadius: 10,
+    marginTop: 20,
+    height: 55,
+    width: 150,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 7,
+    shadowColor: "blue",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
